d178405708a2410fb5213bf8b99b2925fa491ad5
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-09-13 12:29:39 +0300

    SoftUni Advanced tasks

--- lab_lists_as_stacks_and_queues/hot_potato.py
+++ lab_lists_as_stacks_and_queues/hot_potato.py
@@ -2,11 +2,10 @@ from collections import deque
 
 kids_name = deque(input().split())
 
-n = int(input())
-rotate_n = - n + 1
+n = int(input()) - 1
 
 while len(kids_name) > 1:
-    kids_name.rotate(rotate_n)
+    kids_name.rotate(-n)
     removed_kid = kids_name.popleft()
     print(f"Removed {removed_kid}")
 
