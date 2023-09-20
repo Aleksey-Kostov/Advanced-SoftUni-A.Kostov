6c9f2f5358334163664fa451ffe8a72e975b4efd
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-09-20 16:59:08 +0300

    SoftUni Advanced tasks

--- exercise_stacks_queues_tuples_and_sets/01_numbers.py
+++ exercise_stacks_queues_tuples_and_sets/01_numbers.py
@@ -6,4 +6,16 @@ sequences_two = deque(set(int(x) for x in input().split()))
 n = int(input())
 
 for i in range(n):
-    command = input().split()
+    data = input().split()
+    current_command = data[0] + " " + data[1]
+    numbers_data = data[2:]
+    if current_command == "Add First":
+        pass
+    elif current_command == "Add Second ":
+        pass
+    elif current_command == "Remove First":
+        pass
+    elif current_command == "Remove Second":
+        pass
+    elif current_command == "Check Subset":
+        pass
