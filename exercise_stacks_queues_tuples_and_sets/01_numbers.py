ec421bea68a0510d8ecf8e427e7793505b58e08a
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-09-21 10:59:35 +0300

    SoftUni Advanced tasks

--- exercise_stacks_queues_tuples_and_sets/01_numbers.py
+++ exercise_stacks_queues_tuples_and_sets/01_numbers.py
@@ -1,21 +1,22 @@
-from collections import deque
-
-sequences_one = deque(set(int(x) for x in input().split()))
-sequences_two = deque(set(int(x) for x in input().split()))
+sequences_one = set(int(x) for x in input().split())
+sequences_two = set(int(x) for x in input().split())
 
 n = int(input())
 
 for i in range(n):
     data = input().split()
     current_command = data[0] + " " + data[1]
-    numbers_data = data[2:]
+    numbers_data = [int(x) for x in data[2:]]
     if current_command == "Add First":
-        pass
-    elif current_command == "Add Second ":
-        pass
+        sequences_one.update(numbers_data)
+    elif current_command == "Add Second":
+        sequences_two.update(numbers_data)
     elif current_command == "Remove First":
-        pass
+        sequences_one.difference_update(numbers_data)
     elif current_command == "Remove Second":
-        pass
+        sequences_two.difference_update(numbers_data)
     elif current_command == "Check Subset":
-        pass
+        print(sequences_one.issubset(sequences_two) or sequences_two.issubset(sequences_one))
+
+print(*sorted(sequences_one), sep=", ")
+print(*sorted(sequences_two), sep=", ")
