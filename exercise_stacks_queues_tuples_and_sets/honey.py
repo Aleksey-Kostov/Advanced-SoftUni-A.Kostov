5dab389843bf15d49f75924ce72c4a5e6dc9df60
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-09-22 01:43:08 +0300

    SoftUni Advanced tasks

--- exercise_stacks_queues_tuples_and_sets/honey.py
+++ exercise_stacks_queues_tuples_and_sets/honey.py
@@ -8,29 +8,28 @@ symbols = deque(input().split())
 total_honey = 0
 
 while nectar and bees:
-    if bees[0] <= nectar[-1]:
-        current_nectar = nectar.pop()
-        current_bee = bees.popleft()
+    current_nectar = nectar.pop()
+    current_bee = bees.popleft()
+    if current_bee > current_nectar:
+        bees.appendleft(current_bee)
+        continue
+    if current_nectar > 0:
         symbol = symbols.popleft()
         if symbol == "+":
             current_honey = current_nectar + current_bee
             total_honey += abs(current_honey)
         elif symbol == "-":
-            current_honey = current_nectar - current_bee
+            current_honey = current_bee - current_nectar
             total_honey += abs(current_honey)
         elif symbol == "*":
             current_honey = current_nectar * current_bee
             total_honey += abs(current_honey)
         elif symbol == "/":
-            if current_nectar == 0:
-                continue
-            current_honey = current_nectar / current_bee
+            current_honey = current_bee / current_nectar
             total_honey += abs(current_honey)
-    elif bees[0] > nectar[-1]:
-        nectar.pop()
 
 
-print(f"Total honey made: {total_honey:.0f}")
+print(f"Total honey made: {total_honey}")
 if bees:
     print(f"Bees left: {', '.join(str(x) for x in bees)}")
 if nectar:
