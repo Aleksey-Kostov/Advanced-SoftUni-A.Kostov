1bb768ec5eab56b995ae08ac16565334b994a502
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-09-24 08:02:49 +0300

    SoftUni Advanced tasks

--- lab_multidimensional_lists/sum_matrix_elements.py
+++ lab_multidimensional_lists/sum_matrix_elements.py
@@ -2,4 +2,6 @@ rows, col = [int(x) for x in input().split(", ")]
 
 matrix = [[int(j) for j in input().split(", ")] for _ in range(rows)]
 sum_matrix = 0
-print([[sum_matrix + i for i in col] for j in matrix])
+
+print(sum([sum(i) for i in matrix]))
+print(matrix)
