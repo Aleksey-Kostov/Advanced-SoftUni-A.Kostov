fd8815fb1b22b80654a80a7edd450b9921f28b7c
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-09-25 14:45:01 +0300

    SoftUni Advanced tasks

---
+++  exercise_multidimensional_lists/snake_moves.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+rows, cols = [int(x) for x in input().split()]
+
+text = deque(input())
+matrix = []
+
+for row in range(rows):
+    matrix.append([""] * cols)
+    for col in range(cols):
+        if row % 2 == 0:
+            matrix[row][col] = text[0]
+        else:
+            matrix[row][-1 - col] = text[0]
+        text.rotate(-1)
+
+[print(*r, sep="") for r in matrix]
