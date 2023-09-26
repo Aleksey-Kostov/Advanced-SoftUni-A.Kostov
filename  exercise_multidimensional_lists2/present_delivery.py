5e3ed5b9c05db84dcfaa618cd3daf799d5a0ff08
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-09-26 23:36:09 +0300

    SoftUni Advanced tasks

---  exercise_multidimensional_lists2/present_delivery.py
+++  exercise_multidimensional_lists2/present_delivery.py
@@ -21,3 +21,22 @@ for i in range(n_size_neighborhood):
 
 directions_dict = {"left": [0, -1], "right": (0, 1), "up": (-1, 0), "down": (1, 0)}
 
+command = input().split()
+
+while command[0] != "Christmas":
+    r = position[0] + directions_dict[command[0]][0]
+    c = position[1] + directions_dict[command[0]][1]
+    while 0 <= r < n_size_neighborhood and 0 <= c < n_size_neighborhood:
+        if matrix[r][c] == "V":
+            m -= 1
+            position = [r, c]
+            r = position[0] + directions_dict[command[0]][0]
+            c = position[1] + directions_dict[command[0]][1]
+        elif matrix[r][c] == "C":
+            pass
+        else:
+            position = [r, c]
+            r = position[0] + directions_dict[command[0]][0]
+            c = position[1] + directions_dict[command[0]][1]
+    command = input().split()
+    x=5
