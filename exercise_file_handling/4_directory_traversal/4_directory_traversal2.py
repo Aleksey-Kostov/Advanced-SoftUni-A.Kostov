8c1bc3b726aa541a46650098bbb813c779448ef3
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-09 12:41:15 +0300

    SoftUni Advanced tasks

---
+++ exercise_file_handling/4_directory_traversal/4_directory_traversal2.py
@@ -0,0 +1,28 @@
+import os
+
+files = {}
+
+directory = input()
+
+
+def get_file(folder, level):
+    if level < 0:
+        return
+    for element in os.listdir(folder):
+        f = os.path.join(folder, element)
+        if os.path.isfile(f):
+            extension = element.split('.')[-1]
+            if extension not in files:
+                files[extension] = []
+            files[extension].append(element)
+        else:
+            get_file(f, level - 1)
+
+
+get_file(directory, 1)
+
+with open(os.path.join(directory, 'report.txt'), 'w') as output_file:
+    for ext, f_names in sorted(files.items()):
+        output_file.write(f'.{ext}\n')
+        for f_name in sorted(f_names):
+            output_file.write(f'- - - {f_name}\n')
