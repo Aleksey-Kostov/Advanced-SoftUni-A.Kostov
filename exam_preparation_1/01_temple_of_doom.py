5e69bdeadb48e40ee01363fe5d4ba7b39183353e
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-13 21:41:52 +0300

    SoftUni Advanced tasks

--- exam_preparation_1/01_temple_of_doom.py
+++ exam_preparation_1/01_temple_of_doom.py
@@ -5,4 +5,19 @@ substances_sequence = [int(x) for x in input().split()]
 
 check_result = [int(x) for x in input().split()]
 
-x = 5
+while substances_sequence and check_result:
+    tools_number = tools_sequence.popleft()
+    substances_number = substances_sequence.pop()
+    current_result = tools_number * substances_number
+    if current_result in check_result:
+        check_result.remove(current_result)
+    else:
+        tools_number += 1
+        tools_sequence.append(tools_number)
+        substances_number -= 1
+        if substances_number != 0:
+            substances_sequence.append(substances_number)
+
+print(tools_sequence)
+print(check_result)
+
