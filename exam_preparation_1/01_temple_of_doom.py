e32c0df7ea24792093bbdf9d07fe28faaf17a5a9
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-13 21:11:50 +0300

    SoftUni Advanced tasks

---
+++ exam_preparation_1/01_temple_of_doom.py
@@ -0,0 +1,8 @@
+from collections import deque
+
+tools_sequence = deque(int(x) for x in input().split())
+substances_sequence = [int(x) for x in input().split()]
+
+check_result = [int(x) for x in input().split()]
+
+x = 5
