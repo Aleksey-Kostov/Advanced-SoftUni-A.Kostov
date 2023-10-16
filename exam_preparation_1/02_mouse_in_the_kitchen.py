c65a83fcac9344405ceb4faa6a79407413885b56
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-16 16:12:36 +0300

    SoftUni Advanced tasks

--- exam_preparation_1/02_mouse_in_the_kitchen.py
+++ exam_preparation_1/02_mouse_in_the_kitchen.py
@@ -1,11 +1,58 @@
-def matrix_is_valid(r, c, n):
-    if r < 0 or r >= n or c < 0 or c >= n:
-        return True
-    return False
+def is_inside(rows, cols, first_row, first_col):
+    return 0 <= rows < first_row and 0 <= cols < first_col
 
 
-row, cow = [int(x) for x in input().split(",")]
+row, col = [int(x) for x in input().split(",")]
 
-matrix = [[j for j in input()] for i in range(row)]
+matrix = []
+mouse_coord = []
+cheese = []
+
+for i in range(row):
+    matrix.append([x for x in input()])
+    for j in range(col):
+        if matrix[i][j] == "M":
+            mouse_coord = [i, j]
+        if matrix[i][j] == "C":
+            cheese.append((i, j))
 
 mapper = {"left": (0, -1), "right": (0, 1), "down": (1, 0), "up": (-1, 0)}
+
+command = input()
+
+while command != "danger":
+    r = mouse_coord[0] + mapper[command][0]
+    c = mouse_coord[1] + mapper[command][1]
+    if not is_inside(r, c, row, col):
+        print("No more cheese for tonight!")
+        [print("".join(row)) for row in matrix]
+        break
+    matrix[mouse_coord[0]][mouse_coord[1]] = "*"
+    mouse_coord = [r, c]
+    if matrix[r][c] == "C":
+        cheese.remove((r, c))
+        matrix[r][c] = "*"
+        if len(cheese) == 0:
+            matrix[r][c] = "M"
+            print("Happy mouse! All the cheese is eaten, good night!")
+            [print("".join(row)) for row in matrix]
+            break
+    elif matrix[r][c] == "*":
+        matrix[r][c] = "M"
+        mouse_coord = [r, c]
+    elif matrix[r][c] == "@":
+        r = mouse_coord[0] - mapper[command][0]
+        c = mouse_coord[1] - mapper[command][1]
+        mouse_coord = [r, c]
+    elif matrix[r][c] == "T":
+        matrix[r][c] = "M"
+        mouse_coord = [r, c]
+        print("Mouse is trapped!")
+        [print("".join(row)) for row in matrix]
+        break
+    command = input()
+    if command == "danger":
+        print("Mouse will come back later!")
+        [print("".join(row)) for row in matrix]
+
+
