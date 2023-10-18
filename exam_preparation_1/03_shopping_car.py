732bcdfabef80d4a1a4c140f1a17debdffc97100
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-18 16:03:31 +0300

    SoftUni Advanced tasks

--- exam_preparation_1/03_shopping_car.py
+++ exam_preparation_1/03_shopping_car.py
@@ -1,21 +1,40 @@
 def shopping_cart(*args):
-    meal_dict = {}
+    meal_dict = {"Soup": [], "Pizza": [], "Dessert": []}
+    result = ""
+    counter = 0
     for meal in args:
         if meal != "Stop":
             meals = meal[0]
             product = meal[1]
-            if not meals in meal_dict:
-                meal_dict[meals] = []
-            meal_dict[meals].append(product)
+            if len(meal_dict["Soup"]) <= 2 and meals == "Soup":
+                if product not in meal_dict["Soup"]:
+                    meal_dict[meals].append(product)
+                    counter += 1
+            if len(meal_dict["Pizza"]) <= 3 and meals == "Pizza":
+                if product not in meal_dict["Pizza"]:
+                    meal_dict[meals].append(product)
+                    counter += 1
+            if len(meal_dict["Dessert"]) <= 1 and meals == "Dessert":
+                if product not in meal_dict["Dessert"]:
+                    meal_dict[meals].append(product)
+                    counter += 1
+        else:
+            break
+    if counter == 0:
+        return "No products in the cart!"
+    sorted_result = sorted(meal_dict.items(), key=lambda x: (-len(x[1]), x[0]))
+    for i in sorted_result:
+        result += f"{i[0]}:\n"
+        sort_result = sorted(i[1])
+        for j in sort_result:
+            result += f" - {j}\n"
+    return result
 
 
 print(shopping_cart(
-    ('Pizza', 'ham'),
-    ('Soup', 'carrots'),
-    ('Pizza', 'cheese'),
-    ('Pizza', 'flour'),
-    ('Dessert', 'milk'),
-    ('Pizza', 'mushrooms'),
-    ('Pizza', 'tomatoes'),
     'Stop',
+    ('Pizza', 'ham'),
+    ('Pizza', 'mushrooms'),
 ))
+
+
