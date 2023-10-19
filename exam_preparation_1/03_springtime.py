d7bdfb0d728e6631b08ebb6a3293bfec7086cefc
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-19 18:36:07 +0300

    SoftUni Advanced tasks

--- exam_preparation_1/03_springtime.py
+++ exam_preparation_1/03_springtime.py
@@ -7,16 +7,19 @@ def start_spring(**kwargs):
     result = ""
     item_dict = sorted(item_dict.items(), key=lambda x: (-len(x[1]), x[0]))
     for k, v in item_dict:
-        result += (f"{k}:\n"
-        for i in v:
-            result += f"-{sorted(v)}\n"
+        result += f"{k}:\n"
+        for i in sorted(v):
+            result += f"-{i}\n"
     return result
 
 
-example_objects = {"Water Lilly": "flower",
-                   "Swifts": "bird",
-                   "Callery Pear": "tree",
-                   "Swallows": "bird",
-                   "Dahlia": "flower",
-                   "Tulip": "flower",}
+example_objects = {"Magnolia": "tree",
+                   "Swallow": "bird",
+                   "Thrushes": "bird",
+                   "Pear": "tree",
+                   "Cherries": "tree",
+                   "Shrikes": "bird",
+                   "Butterfly": "insect"}
 print(start_spring(**example_objects))
+
+
