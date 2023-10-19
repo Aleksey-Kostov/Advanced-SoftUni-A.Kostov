532222211444d1a013941278406e30907191eea2
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-19 16:12:33 +0300

    SoftUni Advanced tasks

--- exam_preparation_1/02_pawn_wars.py
+++ exam_preparation_1/02_pawn_wars.py
@@ -2,10 +2,9 @@ n = 8
 matrix = []
 coord_w = []
 coord_b = []
-counter_white = 0
-counter_black = 0
 win_white = False
 win_black = False
+counter = 0
 chessboard_matrix = [[chr(97 + j) + str(n - i) for j in range(n)] for i in range(n)]
 
 for i in range(n):
@@ -16,47 +15,72 @@ for i in range(n):
         elif matrix[i][j] == "b":
             coord_b.append((i, j))
 
-mapper = [(0, -1), (-1, -1), (0, 1), (1, 1), (1, 0), (1, -1), (-1, 0), (-1, 1)]
+mapper_white = [(-1, -1), (-1, 1)]
+mapper_black = [(1, 1), (1, -1)]
+
+# mapper = [(0, -1), (-1, -1), (0, 1), (1, 1), (1, 0), (1, -1), (-1, 0), (-1, 1)]
 
 
-def move(coord_white, coord_black, current_maper, matrix_input, matrix_chess):
-    white_win = False
-    black_win = False
-    for current_move in current_maper:
-        r_w = coord_white[0][0] + current_move[0]
+def move_black(coord_black, black_mapper,  matrix_input, matrix_chess, size):
+    for current_move in black_mapper:
+        c_b = coord_black[0][1] + current_move[1]
         r_b = coord_black[0][0] + current_move[0]
-        c_w = coord_white[0][1] + current_move[1]
-        c_b = coord_white[0][1] + current_move[1]
-        if matrix_input[r_w][c_w] == "b":
-            return f"Game over! White win, capture on {matrix_chess[r_w][c_w]}."
+        if not 0 <= c_b < size or not 0 <= r_b < size:
+            continue
         if matrix_input[r_b][c_b] == "w":
             return f"Game over! Black win, capture on {matrix_chess[r_b][c_b]}."
     return False
 
 
-for i in range(n):
-    if not move(coord_w, coord_b, mapper, matrix, chessboard_matrix):
-        r_white = coord_w[0][0] + 0
-        r_black = coord_b[0][0] + 0
-        c_white = coord_w[0][1] + 1
-        c_black = coord_w[0][1] - 1
-        coord_w = [(r_white, c_white)]
-        coord_b = [(r_black, c_black)]
-        if r_white < 7:
-            counter_white += 1
-        elif r_white == 7:
-            counter_white += 1
-            win_white = True
-            break
-        if r_black > 0:
-            counter_black += 1
-        elif r_black == 0:
-            counter_white += 1
-            win_black = True
-            break
+def move_white(coord_white, white_maper, matrix_input, matrix_chess, size):
+    for current_move in white_maper:
+        r_w = coord_white[0][0] + current_move[0]
+        c_w = coord_white[0][1] + current_move[1]
+        if not 0 <= r_w < size or not 0 <= c_w < size:
+            continue
+        if matrix_input[r_w][c_w] == "b":
+            return f"Game over! White win, capture on {matrix_chess[r_w][c_w]}."
+    return False
+
+
+for i in range(n + n):
+    counter += 1
+    if not move_white(coord_w, mapper_white, matrix, chessboard_matrix, n):
+        if counter % 2 != 0:
+            if coord_w[0][0] == 0:
+                win_white = True
+                break
+            r_white = coord_w[0][0] - 1
+            c_white = coord_w[0][1]
+            if 0 <= r_white < n and 0 <= c_white < n:
+                matrix[r_white][c_white] = "w"
+                matrix[coord_w[0][0]][coord_w[0][1]] = "-"
+                coord_w = [(r_white, c_white)]
+                if r_white == 0:
+                    win_white = True
+                    break
     else:
-        move(coord_w, coord_b, mapper, matrix, chessboard_matrix)
+        print(move_white(coord_w, mapper_white, matrix, chessboard_matrix, n))
+        break
+    if not move_black(coord_b, mapper_black, matrix, chessboard_matrix, n):
+        if counter % 2 == 0:
+            if coord_b[0][0] == 7:
+                win_black = True
+                break
+            c_black = coord_b[0][1]
+            r_black = coord_b[0][0] + 1
+            if 0 <= r_black < n and 0 <= c_black < n:
+                matrix[r_black][c_black] = "w"
+                matrix[coord_b[0][0]][coord_b[0][1]] = "-"
+                coord_b = [(r_black, c_black)]
+                if r_black == 7:
+                    win_black = True
+                    break
+    else:
+        print(move_black(coord_b, mapper_black, matrix, chessboard_matrix, n))
         break
 
-print(counter_black)
-print(counter_white)
+if win_white:
+    print(f"Game over! White pawn is promoted to a queen at {chessboard_matrix[coord_w[0][0]][coord_w[0][1]]}.")
+if win_black:
+    print(f"Game over! Black pawn is promoted to a queen at {chessboard_matrix[coord_b[0][0]][coord_b[0][1]]}.")
