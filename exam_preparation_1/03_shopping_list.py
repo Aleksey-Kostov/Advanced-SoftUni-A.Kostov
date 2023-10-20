1504cfb4ffc1a208d8f9fdd6bf16732b9f510c17
Aleksey Kostov <aleksey_kostov@abv.bg>
2023-10-20 09:46:33 +0300

    SoftUni Advanced tasks

--- exam_preparation_1/03_shopping_list.py
+++ exam_preparation_1/03_shopping_list.py
@@ -1,15 +1,32 @@
 def shopping_list(budget, **kwargs):
-    while True:
-        if len(kwargs) >= 5:
-            for i in range(5):
-                for item in kwargs.items():
-                    amount = float(item[0]) * int(item[1])
-        else:
-            pass
+    amount = 0
+    counter = 0
+    result = ""
+    if budget < 100:
+        return "You do not have enough budget."
+    else:
+        for key, value in kwargs.items():
+            price = float(value[0])
+            quantity = int(value[1])
+            amount = price * quantity
+            if budget < amount:
+                continue
+            else:
+                budget -= amount
+                counter += 1
+                if counter == 6:
+                    break
+                result += f"You bought {key} for {amount:.2f} leva.\n"
+        return result
 
 
-print(shopping_list(100,
-                    microwave=(70, 2),
-                    skirts=(15, 4),
-                    coffee=(1.50, 10),
+print(shopping_list(104,
+                    cola=(1.20, 2),
+                    candies=(0.25, 15),
+                    bread=(1.80, 1),
+                    pie=(10.50, 5),
+                    tomatoes=(4.20, 1),
+                    milk=(2.50, 2),
+                    juice=(2, 3),
+                    eggs=(3, 1),
                     ))
